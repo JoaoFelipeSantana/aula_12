c09f76ebcb66f2c4ed7a4eee7de79967a55ece8a
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-23 12:00:39 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Glaceon.java
+++ aula_12_polimorfismo/src/Glaceon.java
@@ -1,3 +1,30 @@
 public class Glaceon extends Eevee{
-    
+    @Override
+    public String Attack() {
+        return "Frost Breath";
+    }
+
+    @Override
+    public String Defense() {
+        return "Avalanche";
+    }
+
+    @Override
+    public String Special() {
+        return "Ice Beam";
+    }
+
+    @Override
+    public void dataSheet() {
+        System.out.println("---------- Glaceon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("-----------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("-----------------------------");
+    }
 }
