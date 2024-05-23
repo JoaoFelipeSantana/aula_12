fedb0a1c083fc4e397f692d7d14d1edea893e838
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-23 11:46:29 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Vaporeon.java
+++ aula_12_polimorfismo/src/Vaporeon.java
@@ -1,3 +1,30 @@
 public class Vaporeon extends Eevee{
-    
+    @Override
+    public String Attack() {
+        return "Water Gun";
+    }
+
+    @Override
+    public String Defense() {
+        return "Aqua Tail";
+    }
+
+    @Override
+    public String Special() {
+        return "Hidro Pump";
+    }
+
+    @Override
+    public void dataSheet() {
+        System.out.println("---------- Vaporeon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("------------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("------------------------------");
+    }
 }
