bb363c63a3fad72e89dc0a653539fbdd29a3959a
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-22 22:33:53 -0300

    [feat] - Adicionando o método "dataSheet"

--- aula_12_polimorfismo/src/Eevee.java
+++ aula_12_polimorfismo/src/Eevee.java
@@ -64,5 +64,18 @@ public class Eevee {
     public String Special() {
         return "Tri_attack";
     }
+
+    public void dataSheet() {
+        System.out.println("---------- Eevee ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("---------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("---------------------------");
+    }
     
 }
