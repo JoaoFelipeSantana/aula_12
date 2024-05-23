07294e41f93b428d858d897a257ef4f144376ade
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-23 11:38:23 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Espeon.java
+++ aula_12_polimorfismo/src/Espeon.java
@@ -1,3 +1,26 @@
-public class Espeon {
-    
+public class Espeon extends Eevee{
+    public String Attack() {
+        return "Psychic";
+    }
+
+    public String Defense() {
+        return "Confusion";
+    }
+
+    public String Special() {
+        return "Future Sight";
+    }
+
+    public void dataSheet() {
+        System.out.println("---------- Espeon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("----------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("----------------------------");
+    }
 }
