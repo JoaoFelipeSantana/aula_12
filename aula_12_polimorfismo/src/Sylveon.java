566f2b7982b4d2899b8c7f2bb30f244577dd7408
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-22 23:17:24 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Sylveon.java
+++ aula_12_polimorfismo/src/Sylveon.java
@@ -1,3 +1,26 @@
 public class Sylveon extends Eevee{
-    
+    public String Attack() {
+        return "Charm";
+    }
+
+    public String Defense() {
+        return "Dazzling Gleam";
+    }
+
+    public String Special() {
+        return "Moonblast";
+    }
+
+    public void dataSheet() {
+        System.out.println("---------- Sylveon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("-----------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("-----------------------------");
+    }
 }
