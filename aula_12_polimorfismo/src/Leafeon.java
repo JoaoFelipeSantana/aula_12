38195f9521eee3af2ca04d4689fbe50d07cbd5a6
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-23 11:52:26 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Leafeon.java
+++ aula_12_polimorfismo/src/Leafeon.java
@@ -1,3 +1,30 @@
 public class Leafeon extends Eevee{
-    
+    @Override
+    public String Attack() {
+        return "Quick Attack";
+    }
+
+    @Override
+    public String Defense() {
+        return "Bullet Speed";
+    }
+
+    @Override
+    public String Special() {
+        return "Solar Beam";
+    }
+
+    @Override
+    public void dataSheet() {
+        System.out.println("---------- Leafeon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("-------------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("-------------------------------");
+    }
 }
