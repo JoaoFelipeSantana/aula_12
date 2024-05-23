7cdbd49f07f66cc966c09b49ea749090cfc74eb9
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-22 23:10:06 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Jolteon.java
+++ aula_12_polimorfismo/src/Jolteon.java
@@ -1,3 +1,30 @@
 public class Jolteon extends Eevee{
-    
+    @Override
+    public String Attack() {
+        return "Thunder Shock";
+    }
+
+    @Override
+    public String Defense() {
+        return "Volt Switch";
+    }
+
+    @Override
+    public String Special() {
+        return "Thunderbolt";
+    }
+
+    @Override
+    public void dataSheet() {
+        System.out.println("---------- Jolteon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("-----------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("-----------------------------");
+    }
 }
