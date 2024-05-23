d6fe74385ddaf9105bbc51c717d2cbeaeacdbc45
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-23 11:41:43 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Floreon.java
+++ aula_12_polimorfismo/src/Floreon.java
@@ -1,3 +1,26 @@
 public class Floreon extends Eevee{
-    
+    public String Attack() {
+        return "Ember";
+    }
+
+    public String Defense() {
+        return "Fire Spin";
+    }
+
+    public String Special() {
+        return "Overheat";
+    }
+
+    public void dataSheet() {
+        System.out.println("---------- Floreon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("-----------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("-----------------------------");
+    }
 }
