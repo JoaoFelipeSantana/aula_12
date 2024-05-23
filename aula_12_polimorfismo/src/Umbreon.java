315456a44e12c57f1286f08a80858fda0c2e064d
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-23 11:28:04 -0300

    [feat] - Sobrescrevendo os métodos da superclasse

--- aula_12_polimorfismo/src/Umbreon.java
+++ aula_12_polimorfismo/src/Umbreon.java
@@ -1,3 +1,26 @@
 public class Umbreon extends Eevee{
-    
+    public String Attack() {
+        return "Feint Attack";
+    }
+
+    public String Defense() {
+        return "Snarl";
+    }
+
+    public String Special() {
+        return "Dark Pulse";
+    }
+
+    public void dataSheet() {
+        System.out.println("---------- Umbreon ----------");
+        System.out.println("Type: " + getType());
+        System.out.printf("Weight: %.2f\n", getWeight());
+        System.out.printf("Height: %.2f\n", getHeight());
+        System.out.println("HP: " + getHp());
+        System.out.println("-----------------------------");
+        System.out.println("Attack: " + Attack());
+        System.out.println("Special: " + Special());
+        System.out.println("Defense: " + Defense());
+        System.out.println("-----------------------------");
+    }
 }
