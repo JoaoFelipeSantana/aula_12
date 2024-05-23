e69bda50ad884a2862e3b845187caa3ebee6011a
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-22 22:21:02 -0300

    [feat] - Criando a classe "Eevee"
    - Criando os atributos;
    - Encapsulando a classe.

---
+++ aula_12_polimorfismo/src/Eevee.java
@@ -0,0 +1,55 @@
+public class Eevee {
+    
+    // Atributos
+    String type;
+    double weight;
+    int height;
+    int hp;
+
+    // Contrutor
+    public Eevee() {
+
+    }
+
+    public Eevee(String type, double weight, int height, int hp) {
+        this.type = type;
+        this.weight = weight;
+        this.height = height;
+        this.hp = hp;
+    }
+
+    // Métodos acessores
+    public String getType() {
+        return type;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getHp() {
+        return hp;
+    }    
+
+    // Métodos modificadores
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    
+}
